d7020203c84e13d08825c907b209addf2fb342c0
glingg <gregory@boys-mbp.attlocal.net>
2016-09-22 21:27:19 -0700

    revert get

--- Memorizer/js/js.js
+++ Memorizer/js/js.js
@@ -63,9 +63,13 @@ function createtest() {
   if (testArray[testArray.length - 1] === "") {
     testArray.pop();
   }
-  if (/[a-z]|[A-Z]|[0-9]/.test(testArray[0].split('')[0]) && testArray[0] != "") {
+  if (testArray[0] === "") {
+    testArray.shift();
+  }
+  if (/[a-zA-Z0-9\u00C0-\u02B8\u0388-\u0556]/.test(testArray[0].split('')[0]) && testArray[0] != "") {
     testArray.unshift("");
   }
+  console.log(testArray);
   addblanks();
   console.log(testArray);
   setTimeout(function() {
@@ -148,25 +152,6 @@ function isGrammarly() {
 //init preset array
 var presets = [""];
 
-//get get preset if any //from css-tricks.com
-function getQueryVariable(variable)
-{
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if(pair[0] == variable){return pair[1];}
-  }
-  return(false);
-}
-
-function getget() {
-  var pre = getQueryVariable("preset");
-  if (pre) {
-    document.getElementById('input').value = pre;
-  }
-}
-
 //load presets
 function loadpresets(arr) {
   var wo;
@@ -177,7 +162,6 @@ function loadpresets(arr) {
     presets.push(arr[i].preset);
     document.getElementById("preset").appendChild(wo);
   }
-  getget();
 
 }
 
@@ -185,12 +169,4 @@ function loadpresets(arr) {
 function presetc() {
   var pnum = parseInt(document.getElementById("preset").value);
   document.getElementById("input").value = presets[pnum];
-}
-
-//create link to entered preset
-function createlink() {
-  var go = prompt('You will be redirected to a page with the url of the preset. Copy and use that url to get to that preset. Enter "no" to stop.');
-  if (go != "no") {
-    document.getElementById('form').submit();
-  }
 }
