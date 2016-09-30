a006905dec1b9ee85d1d2ab4a39349b3ee167a9d
glingg <gregory@boys-mbp.attlocal.net>
2016-09-30 16:32:05 -0700

    add mem. verse

--- Memorizer/js/presets.js
+++ Memorizer/js/presets.js
@@ -10,5 +10,9 @@
   {
     "name" : "AccentTest",
     "preset" : "Thîs îs Å tést... Whát îs thís?"
+  },
+  {
+    "name" : "Memory Verse",
+    "preset" : "\"So now there is no condemnation for those who belong to Christ Jesus. And because you belong to him, the power of the life-giving spirit has freed you from the power of sin that leads to death.\" - Romans 8:1-2"
   }
 ]), 1000))
