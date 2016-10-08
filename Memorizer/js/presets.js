1efe29b0fb48d12f6addae93a102827bc3c21506
glingy <gregory.ling@mba-1076.attlocal.net>
2016-10-08 10:41:58 -0700

    add neg. preset

--- Memorizer/js/presets.js
+++ Memorizer/js/presets.js
@@ -6,5 +6,13 @@
   {
     "name" : "Memory Verse",
     "preset" : "\"So now there is no condemnation for those who belong to Christ Jesus. And because you belong to him, the power of the life-giving spirit has freed you from the power of sin that leads to death.\" - Romans 8:1-2"
-  }
+  },
+  {
+    "name" : "Negative 10 Commandments",
+    "preset" : "1. You must not have any other god but me.\n2. You must not make for yourself an idol.\n3. You must not misuse the name of the LORD your God. \n4. Remember to observe the Sabbath day by keeping it holy.\n5. Honor your father and mother. \n6. You must not murder.\n7. You must not commit adultery.\n8. You must not steal.\n9. You must not testify falsely against your neighbor.\n10. You must not covet your neighbor’s house."
+  }/*,
+  {
+    "name" : "Positive 10 Commandments",
+    "preset" : "\"So now there is no condemnation for those who belong to Christ Jesus. And because you belong to him, the power of the life-giving spirit has freed you from the power of sin that leads to death.\" - Romans 8:1-2"
+  }*/
 ]), 1000))
