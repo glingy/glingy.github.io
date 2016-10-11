a9ad5234cbe1c995b3d45a84aa8d8045e3ac0f49
glingy <gregory.ling@MBA-1076.local>
2016-10-11 14:09:20 -0700

    add pos. commandments

--- Memorizer/js/presets.js
+++ Memorizer/js/presets.js
@@ -10,9 +10,9 @@
   {
     "name" : "Negative 10 Commandments",
     "preset" : "1. You must not have any other god but me.\n2. You must not make for yourself an idol.\n3. You must not misuse the name of the LORD your God. \n4. Remember to observe the Sabbath day by keeping it holy.\n5. Honor your father and mother. \n6. You must not murder.\n7. You must not commit adultery.\n8. You must not steal.\n9. You must not testify falsely against your neighbor.\n10. You must not covet your neighbor’s house."
-  }/*,
+  },
   {
     "name" : "Positive 10 Commandments",
-    "preset" : "\"So now there is no condemnation for those who belong to Christ Jesus. And because you belong to him, the power of the life-giving spirit has freed you from the power of sin that leads to death.\" - Romans 8:1-2"
-  }*/
+    "preset" : "1. Honor me as God.\n2. Honor the one true God.\n3. Honor God's name.\n4. Find rest in me.\n5. Honor your father and mother.\n6. Honor life.\n7. Honor marriage and be faithful.\n8. Honor the belongings of others.\n9. Honor truth.\n10. Be content in me."
+  }
 ]), 1000))
