cb0600cd3f93e175b6f9db4e69ade1455ae095c2
glingy <gregory.ling@MBA-1076.local>
2016-10-18 14:14:35 -0700

    add Gospel truths preset

--- Memorizer/js/presets.js
+++ Memorizer/js/presets.js
@@ -14,5 +14,10 @@
   {
     "name" : "Positive 10 Commandments",
     "preset" : "1. Honor me as God.\n2. Honor the one true God.\n3. Honor God's name.\n4. Find rest in me.\n5. Honor your father and mother.\n6. Honor life.\n7. Honor marriage and be faithful.\n8. Honor the belongings of others.\n9. Honor truth.\n10. Be content in me."
+  },
+  {
+    "name" : "GospelTruths-MyChoice",
+    "preset" : "1.\n a. God is our Creator, and He is Lord over all of creation. (Genesis 1:1)\n c. God is completely holy, righteous, and just. He cannot excuse sin. (Romans 3:23)\n\n2.\n a. All people have sinned and fall short of the glory of God. (Romans 3:23)\n b. The wages of sin is death. (Romans 6:23)\n\n3.\n a. Jesus is God the Son who became man to save sinners. (John 1:14)\n b. \"But God demonstrates His love toward us, in that, while we were still sinners, Christ died for us. (Romans 5:8)\n\n4.\n a. When we believe that Jesus Christ died for our sins, we are saved. Salvation comes by grace through faith. \"For it is by grace you have been saved through faith, and that not of yourselves; it is the gift of God, not by works, lest anyone should boast.\" (Ephesians 2:8)\n b. Our reasonable response is to receive salvation through faith in Jesus Christ our Lord. (Romans 10:9-13)"
+
   }
 ]), 1000))
