06b377404b058504c9f63f25869dc7bf02a4dae9
Gregory Ling <0b0100011101101100@gmail.com>
2023-03-16 22:06:01 -0500

    gened reflection done

--- src/router.ts
+++ src/router.ts
@@ -1,14 +1,19 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {createRouter, createWebHistory, RouteRecordRaw} from "vue-router";
 
 import routes from './pages/routes'
 
 let router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes,
+})
+
+router.beforeEach((to, from, next) => {
+    window.scrollTo(0, 0)
+    next()
 })
 
 export default router
 
 export function createRoutes(routes: RouteRecordRaw[]) {
-  return routes
+    return routes
 }
